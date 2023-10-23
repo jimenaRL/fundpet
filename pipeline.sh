7cf7952a9f23376d82e21b6e04956bbae48b0af3
jimenaRL <jimena@melix.net>
2023-10-23 17:16:36 +0200

    Added trafilatura content extration

--- pipeline.sh
+++ pipeline.sh
@@ -9,8 +9,11 @@ echo Preprocessing data for platform $platform
 python preprocessing.py --platform=$platform  --config=preprocessing_config.yaml
 
 # fetch websites
-echo Fetching website of domain $domain
+echo Fetching sites of domain $domain
 minet fetch externalUrl /home/jimena/work/data/fundpet/scrapping/${domain}/toFetchUrl_${platform}__${domain}.csv \
 --output /home/jimena/work/data/fundpet/scrapping/${domain}/fetchedUrl_${platform}__${domain}.csv \
 --output-dir /home/jimena/work/data/fundpet/scrapping/${domain}/fechted \
 --threads 100
+
+# extarct content from fetched sites
+python scrappers/${domain}_scrapper.py
--- scrappers/change.org_scrapper.py
+++ scrappers/change.org_scrapper.py
@@ -1,44 +1,66 @@
 import os
+import json
 from tqdm import tqdm
 from glob import glob
 from bs4 import BeautifulSoup
+import pandas as pd
+import trafilatura
 
-FOLDER = "/home/jimena/work/dev/fundpet/externalUrl"
-paths = glob(os.path.join(FOLDER, '*.html'))
+DOMAIN = 'change.org'
+BASEFOLDER = "/home/jimena/work/data/fundpet/scrapping"
+FOLDER = os.path.join(BASEFOLDER, DOMAIN)
+paths = glob(os.path.join(FOLDER,'fetched', '*.html'))
 uids = [os.path.split(p)[1].replace('.html', '') for p in paths]
-# Open File
+
+outpath = os.path.join(FOLDER, f'extrated_content_{DOMAIN}.csv')
+
 res = {
      uid : {
         'h1': None,
         'h2': None,
         'title': None,
-        'p': []
+        'p': [],
+        'comments': None
     } for uid in uids
 }
 
-for uid in tqdm(uids):
-    path = os.path.join(FOLDER, f'{uid}.html')
+for uid, path in tqdm(zip(uids, paths)):
+
+    # add info with BeautifulSoup
     with open(path) as f:
         soup = BeautifulSoup(f, 'html.parser')
 
-        h1 = soup.h1
-        if h1 is not None:
-            res[uid]['h1'] = h1.text
+    h1 = soup.h1
+    if h1 is not None:
+        res[uid]['h1'] = h1.text
 
-        h2 = soup.h2
-        if h2 is not None:
-            res[uid]['h2'] = h2.text
+    h2 = soup.h2
+    if h2 is not None:
+        res[uid]['h2'] = h2.text
 
-        res[uid]['title'] = soup.title
+    res[uid]['title'] = soup.title.text
 
-        pdivs = soup.find_all('div', class_="corgi-1wj70kl")
+    pdivs = soup.find('div', class_="corgi-1wj70kl")
+    if pdivs is not None:
         if len(pdivs) > 0:
-            if len(pdivs) > 1:
-                raise ValueError('RARO')
-            else:
-                for c in ddiv.childGenerator():
-                    if c.name == 'p':
-                        res[uid]['p'].append(c.text)
-                    else:
-                        raise ValueError('RARITO')
+            for c in pdivs.childGenerator():
+                if c.name == 'p':
+                    res[uid]['p'].append(c.text)
 
+    # add info with Trafilatura
+    with open(path) as f:
+        filecontent = f.read()
+
+    res[uid].update(json.loads(
+        trafilatura.extract(
+            filecontent,
+            favor_recall=True,
+            include_comments=True,
+            include_formatting=True,
+            output_format='json')))
+
+
+df = pd.DataFrame.from_records(data=res)
+df.to_csv(outpath, index=False)
+print(f"Content extracted from {len(df)} sited saved to {outpath}.")
+print(df)
